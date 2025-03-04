209f1e1ec95182be8ed07bc3c5bb7f862af5691f
brunotxrs <elielsonbrunno@gmail.com>
2025-03-04 17:19:59 -0400

    feat: create one function for menu!

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('menu');
+menu.addEventListener('click', () => {
+    console.log('ok')
+    const secondary =  document.getElementById('secondary')
+    secondary.style.display = 'flex'
+})
