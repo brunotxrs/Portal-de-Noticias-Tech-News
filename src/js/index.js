880b88d122d0039eec300dd49006ce1f71f0f748
brunotxrs <elielsonbrunno@gmail.com>
2025-03-07 18:25:19 -0400

    feat: update the interactions with menu !

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,25 @@
-const menu = document.getElementById('menu');
-menu.addEventListener('click', () => {
-    console.log('ok')
-    const secondary =  document.getElementById('secondary')
-    secondary.style.display = 'flex'
-})
+const menuOpen = document.getElementById('menu-open');
+const menuClose = document.getElementById('menu-close');
+const main = document.getElementById('main');
+const secondary = document.getElementById('secondary');
+
+function show(show) {
+
+    if(show) {
+        secondary.style.display = 'flex';
+        menuClose.style.display = 'flex';
+        main.style.display = 'none';
+        menuOpen.style.display = 'none';
+
+        return
+
+    } else {
+        secondary.style.display = 'none';
+        menuClose.style.display = 'none'
+        main.style.display = 'flex';
+        menuOpen.style.display = 'flex';
+    }
+}
+
+menuOpen.addEventListener('click', () => show(true));
+menuClose.addEventListener('click', () => show(false));
